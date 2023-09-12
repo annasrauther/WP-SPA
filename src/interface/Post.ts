f9a7d36bbe7562edb58fc1ab24b43684d5cfbeb0
Annas Rauther <annasrauther95@gmail.com>
2023-09-12 18:08:26 +0530

    feat: add static typing for data sets

---
+++ src/interface/Post.ts
@@ -0,0 +1,6 @@
+export interface PostProps {
+  title: string;
+  datePublished: string;
+  author: string;
+  content: string;
+}
---
+++ src/interface/PostList.ts
@@ -0,0 +1,5 @@
+import { PostProps } from "./Post";
+
+export interface PostListProps {
+  posts: PostProps[];
+}
