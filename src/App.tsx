2e06d0cc76cd4b0875a5a0d5ba9d777c0c1331ed
Annas Rauther <annasrauther95@gmail.com>
2023-09-12 18:09:48 +0530

    feat: implement router with catch-all inside pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+
+// Lazy-load your components using React.lazy
+const Home = React.lazy(() => import("./pages/Home"));
+const Page = React.lazy(() => import("./pages/Page"));
+const Login = React.lazy(() => import("./pages/Login"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pages/:slug" element={<Page />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
